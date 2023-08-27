9d0e32a50e56b8f5db6c8ca463303b6855aa43bd
Oscar G <murdexchannel@gmail.com>
2023-08-27 14:50:54 -0400

    adding logo and changing the headeer layout

--- src/Header.js
+++ src/Header.js
@@ -7,24 +7,51 @@ import { faLinkedin, faGithub, faSquareFacebook} from '@fortawesome/free-brands-
 export const Header = () => {
   return (
     <div className='header-container'>
-      <div className='socials-container'>
-          <a href='https://www.linkedin.com/in/oscar-gomez-77b472195/' className='social-link' target='_blank'>
-            <FontAwesomeIcon icon={faLinkedin} size='2xl' />
-          </a>
-          <a href='https://github.com/oscargomez15' className='social-link' target='_blank'>
-          <FontAwesomeIcon icon={faGithub} size='2xl'/>
-          </a>
-          <a href='https://www.facebook.com/oscargomez1998' target='_blank'>
-          <FontAwesomeIcon icon={faSquareFacebook} size='2xl'/>
-          </a>
+
+      <div className='logo-container'>
+
+        <div className='logo'>
+          <h1 className='O'>O</h1>
+          <div className='logo-divider'></div>
+          <div className='hide-divider'></div>
+          <h1 className='G'>G</h1>
+        </div>
+
+        <div className='name'>
+          <p>Oscar Gomez</p>
+        </div>
+
+        <div className='role'>
+          <p>Web Developer</p>
+        </div>
+
       </div>
 
       <div className='title-container'>
         <h1 className='app-title'> Crypto Tracker </h1>
-        <p> Developed by Oscar Gomez </p>
+        <p> Build your future, invest smart. </p>
       </div>
 
-      <div className='empty-container'></div>
+      <div className='socials-container'>
+          <div className='social'>
+            <a href='https://www.linkedin.com/in/oscar-gomez-77b472195/' className='social-link' target='_blank'>
+              <FontAwesomeIcon icon={faLinkedin} size='2xl' />
+            </a>
+          </div>
+          
+          <div className='social'>
+            <a href='https://github.com/oscargomez15' className='social-link' target='_blank'>
+            <FontAwesomeIcon icon={faGithub} size='2xl'/>
+            </a>
+          </div>
+
+          <div className='social'>
+            <a href='https://www.facebook.com/oscargomez1998' target='_blank'>
+            <FontAwesomeIcon icon={faSquareFacebook} size='2xl'/>
+            </a>
+          </div>
+      </div>
+
     </div>
   )
 }
