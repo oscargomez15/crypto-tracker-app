95fad6656320cb9add4d84ff52f34502d6cc85cc
Oscar G <murdexchannel@gmail.com>
2023-08-27 12:04:44 -0400

    added links to social media icons and fixed the header layout

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import './Header.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faGithub, faSquareFacebook} from '@fortawesome/free-brands-svg-icons'
+
 
 export const Header = () => {
   return (
     <div className='header-container'>
       <div className='socials-container'>
-        <p>LinkedIn</p>
-        <p>GitHub</p>
-        <p>Facebook</p>
+          <a href='https://www.linkedin.com/in/oscar-gomez-77b472195/' className='social-link' target='_blank'>
+            <FontAwesomeIcon icon={faLinkedin} size='2xl' />
+          </a>
+          <a href='https://github.com/oscargomez15' className='social-link' target='_blank'>
+          <FontAwesomeIcon icon={faGithub} size='2xl'/>
+          </a>
+          <a href='https://www.facebook.com/oscargomez1998' target='_blank'>
+          <FontAwesomeIcon icon={faSquareFacebook} size='2xl'/>
+          </a>
       </div>
 
       <div className='title-container'>
@@ -15,6 +24,7 @@ export const Header = () => {
         <p> Developed by Oscar Gomez </p>
       </div>
 
+      <div className='empty-container'></div>
     </div>
   )
 }
